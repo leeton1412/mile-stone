f47f41434a87d65580d0fb4c6bfff90f7897ef0e
leeton1412 <leighton.griffith87@gmail.com>
2021-05-12 20:33:32 +0000

    created form for products

--- holding/contexts.py
+++ holding/contexts.py
@@ -3,15 +3,18 @@ from django.conf import settings
 
 
 def bag_contents(request):
+    """ Context def to store bag items and total """
 
     bag_items = {}
     total = 0
     product_count = 0
+    grand_total = 0
 
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count
+        'product_count': product_count,
+        'grand_total': grand_total
     }
 
     return context
