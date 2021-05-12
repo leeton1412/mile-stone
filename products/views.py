cebd9eda037b292cf67907a0317b3bf8e5bd8105
leeton1412 <leighton.griffith87@gmail.com>
2021-05-12 16:41:14 +0000

    Added price sort

--- products/views.py
+++ products/views.py
@@ -8,30 +8,47 @@ from .models import Product, Category
 
 def all_products(request):
     """Simple View to show all products"""
+    sort = None
+    direction = None
     products = Product.objects.all()
     query = None
     categories = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+                products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-
-
+            
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "No Items Entered")
                 return redirect(reverse('products'))
-           
+          
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+    
+        current_sort = f'{sort}_{direction}'    
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sort': current_sort,
     }
     return render(request, "products/products.html", context)
 
