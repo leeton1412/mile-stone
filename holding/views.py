15be26783edbea75d51eb67fc934817b329e87ee
leeton1412 <leighton.griffith87@gmail.com>
2021-05-13 17:27:23 +0000

    started update bag view

--- holding/views.py
+++ holding/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-
 def bag_view(request):
     """Simple View to render page"""
     return render(request, "holding/shopping-bag.html")
@@ -12,6 +11,20 @@ def add_item(request, item_id):
 
     quantity = int(request.POST.get('amount'))
     redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+
+def update_item(request, item_id):
+    """ View to update items in bag """
+
+    quantity = int(request.POST.get('amount'))
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
