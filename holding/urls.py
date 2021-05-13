b9064e681e7ac0cdab9382f96642bbe1b43121df
leeton1412 <leighton.griffith87@gmail.com>
2021-05-13 12:25:36 +0000

    Built bag_view and tested

--- holding/urls.py
+++ holding/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.bag_view, name='bag_view')
+    path('', views.bag_view, name='bag_view'),
+    path('add/<item_id>', views.add_item, name='add_item'),
 ]
