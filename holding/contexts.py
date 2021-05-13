f0986166f42eac8c713bbe03a1b4855fcc8949ff
leeton1412 <leighton.griffith87@gmail.com>
2021-05-13 13:47:39 +0000

    Added functionality to add items to basket

--- holding/contexts.py
+++ holding/contexts.py
@@ -1,20 +1,33 @@
 from decimal import Decimal
 from django.conf import settings
+from products.models import Product
+from django.shortcuts import get_object_or_404
 
 
 def bag_contents(request):
     """ Context def to store bag items and total """
 
-    bag_items = {}
+    bag_items = []
     total = 0
     product_count = 0
-    grand_total = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
-        'grand_total': grand_total
+        'grand_total': total
     }
 
     return context
