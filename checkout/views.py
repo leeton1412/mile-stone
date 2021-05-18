eff818a70239721a9bf3e2d8288e4ef68ee90fe1
leeton1412 <leighton.griffith87@gmail.com>
2021-05-18 19:22:30 +0000

    Added Stripe and Doc Styling

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IsXGOEXwwcaVeHN4rPgTtOGN5S3PdqHcePqQ5QMHNeLoYcvTVnMopxSVsQoWOX6S55WJoEj3cBRK7EGWqABn2iG00b2KU7SDZ',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
