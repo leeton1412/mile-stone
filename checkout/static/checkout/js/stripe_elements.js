fc04148674ee2b7dc3c2029565a5ace2a1d90cba
leeton1412 <leighton.griffith87@gmail.com>
2021-05-18 20:04:17 +0000

    Added warning, hidden behind elements

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -23,3 +23,19 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Validation Errors
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('#card-errors');
+    if (event.error) {
+        var html = `
+            <span 
+                <i class="fas fa-times></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
