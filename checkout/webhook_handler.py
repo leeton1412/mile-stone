31165354a0034f1c5aa4fbffc082cb1b422ea660
leeton1412 <leighton.griffith87@gmail.com>
2021-05-21 20:25:18 +0000

    added Webhook method

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """ This Handles Stripe Webhooks """
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        return HttpResponse(
+            content=f'Webhook Recieved {event[type]}',
+            status=200)
