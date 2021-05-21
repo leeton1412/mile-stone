b8de88882b61b430554dfd5b3268084a0673b180
leeton1412 <leighton.griffith87@gmail.com>
2021-05-21 22:44:53 +0000

    added webhook database objects

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -52,7 +52,31 @@ form.addEventListener('submit', function(ev) {
     stripe.confirmCardPayment(client_secret, {
         payment_method: {
             card: card,
-        }
+            billing_details:{
+                name: $.trim(form.full_name.value),
+                email:$.trim(form.email.value),
+                phone:$.trim(form.phone_number.value),
+                address:{
+                    line1:$.trim(form.street_address1.value),
+                    line2:$.trim(form.street_address2.value),
+                    city:$.trim(form.town_or_city.value),
+                    county:$.trim(form.county.value),
+                    country:$.trim(form.country.value),
+                }
+            }
+        },
+        shipping: {
+                name: $.trim(form.full_name.value),
+                phone:$.trim(form.phone_number.value),
+                address:{
+                    line1:$.trim(form.street_address1.value),
+                    line2:$.trim(form.street_address2.value),
+                    city:$.trim(form.town_or_city.value),
+                    postcode:$.trim(form.postcode.value),
+                    county:$.trim(form.county.value),
+                    country:$.trim(form.country.value),
+                }
+        },
     }).then(function(result) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
