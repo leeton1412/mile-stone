7c4f5cc673cf2e436fe72d31a4cc00db8b1b8466
leeton1412 <leighton.griffith87@gmail.com>
2021-05-23 17:04:29 +0000

    Added username to template to test profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 # Create your views here.
 
 
 def profiles(request):
-    """Simple View to render page"""
+    """Simple View to render page and display profile"""
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profiles.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
