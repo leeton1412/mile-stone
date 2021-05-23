ed41fa5342b7baaf94f5fd93d48a2956ca388dca
leeton1412 <leighton.griffith87@gmail.com>
2021-05-23 13:34:02 +0000

    Templates tested

--- profiles/views.py
+++ profiles/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def profiles(request):
     """Simple View to render page"""
-    template = 'profiles/profile.html'
+    template = 'profiles/profiles.html'
     context = {}
 
     return render(request, template, context)
