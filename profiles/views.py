c6a895e04e4f83f565ed27b257ae3d13c7fb6ed5
leeton1412 <leighton.griffith87@gmail.com>
2021-05-23 13:18:21 +0000

    Added template profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
-
-# Create your views here.
-
 
 def profiles(request):
     """Simple View to render page"""
-    return render(request, "profiles/profiles.html")
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
