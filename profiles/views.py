9948643ea1e8e80aece51867847da5770dc10ae0
leeton1412 <leighton.griffith87@gmail.com>
2021-05-24 18:38:17 +0000

    Added update function

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 # Create your views here.
@@ -7,6 +8,12 @@ from .forms import UserProfileForm
 def profiles(request):
     """Simple View to render page and display profile"""
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Updated')
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
