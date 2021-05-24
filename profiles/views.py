9a89d9699cbb1a93af11f1023f21d9085e4d94ed
leeton1412 <leighton.griffith87@gmail.com>
2021-05-24 19:15:45 +0000

    Added templates to profile for orders

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import UserProfile
+from checkout.models import Order
 from .forms import UserProfileForm
 # Create your views here.
 
@@ -22,6 +23,18 @@ def profiles(request):
     context = {
         'form': form,
         'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
